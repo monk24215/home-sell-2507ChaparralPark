7567966c5be4743e2b0f11290a814fc5f8125387
monk24215 <koubre@gmail.com>
2026-05-25 12:39:38 -0500

    Change backdrop class addition method

--- backld.js
+++ backld.js
@@ -2,7 +2,7 @@
 // 
 //   document.getElementById('backdrop').className = 'open';
 window.onload = function() {
- document.getElementById('backdrop').classList.add('open');
+ document.getElementById('backdrop').className = 'backdrop.open';
   console.log('grr');
 }
 
