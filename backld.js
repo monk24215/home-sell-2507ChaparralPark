2c7b5f7c7274e3960655c510107e2327e0e8aabe
monk24215 <koubre@gmail.com>
2026-05-25 12:36:36 -0500

    Create backld.js

---
+++ backld.js
@@ -0,0 +1,8 @@
+
+// 
+//   document.getElementById('backdrop').className = 'open';
+window.onload = function() {
+ document.getElementById('backdrop').classList.add('open');
+  console.log('grr');
+}
+
